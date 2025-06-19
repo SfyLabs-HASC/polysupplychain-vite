a13c02018c024220d2daa8834df6033d06589398
SfyLabs-HASC <sfy.startup@gmail.com>
2025-06-19 14:51:50 +0200

    Aggiungo pagina Gestisci e correggo struttura

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// --- MODIFICA --- Ripristinati i percorsi di importazione corretti ---
-// App.tsx si trova in /src, quindi per accedere alle pagine deve entrare in /pages
 import AziendaPage from './pages/AziendaPage';
 import GestisciPage from './pages/GestisciPage'; 
-// App.css è nella stessa cartella di App.tsx, quindi il percorso è semplice
 import './App.css';
 
 function App() {
